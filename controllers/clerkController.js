faef0148074bdf7beaab13198c97ff304effd407
asuraxo <asuraxo@gmail.com>
2023-01-24 23:31:00 -0500

    Burgers A4 + A6 clerk_controller_1

--- controllers/clerkController.js
+++ controllers/clerkController.js
@@ -1,6 +1,8 @@
-const mealkitModel = require("../models/meal-kitModel");
 const express = require("express");
 const router = express.Router();
+const mealkitModel = require("../models/meal-kitModel");
+
+var meals;
 
 router.get("/list-mealkits", checkClerk, (req, res) => {
     res.render("list-mealkits")
