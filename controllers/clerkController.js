ddeeb327fc45bcf2d9e7970db523cf0d417037fa
asuraxo <asuraxo@gmail.com>
2023-01-24 23:48:43 -0500

    Burgers A4 + A6 clerk_controller_addMealKit

--- controllers/clerkController.js
+++ controllers/clerkController.js
@@ -29,6 +29,57 @@ router.get("/list-mealkits", checkClerk, (req, res) => {
     });
 });
 
+router.post("/add-mealkit", checkClerk, (req, res) => {
+
+    req.body.isTopMeal ? req.body.isTopMeal=true : req.body.isTopMeal=false;
+
+    let newMeal = new mealkitModel({
+        title       : req.body.title,
+        includes    : req.body.includes,
+        description : req.body.description,
+        category    : req.body.category,
+        price       : req.body.price,
+        cookingTime : req.body.cookingTime,
+        servings    : req.body.servings,
+        topMeal     : req.body.isTopMeal
+    });
+
+    newMeal.save()
+        .then(mealSaved => {
+            console.log(`Meal: ${mealSaved.title} has been added to the database.`);
+
+            // Create a unique name for the image, so that it can be stord in the file system.
+            let uniqueName = `${mealSaved._id}${path.parse(req.files.mealPic.name).ext}`;
+
+            // Copy the image data to a file in the "/assets/profile-pics" folder.
+            req.files.mealPic.mv(`assets/images/Burgers/${uniqueName}`)
+                .then(() => {
+                    // Update the document so it includes the unique name.
+                    mealkitModel.updateOne({
+                        _id: mealSaved._id
+                    }, {
+                        imageUrl: uniqueName
+                    })
+                        .then(() => {
+                            // Success
+                            console.log("Updated the meal pic.");
+                            res.redirect("/clerk/list-mealkits");
+                        })
+                        .catch(err => {
+                            console.log(`Error updating the burger's image ... ${err}`);
+                            res.redirect("/clerk/list-mealkits");
+                        });
+                })
+                .catch(err => {
+                    console.log(`Error saving the burger's picture ... ${err}`);
+                    res.redirect("/clerk/list-mealkits");
+                });
+        })
+        .catch(err => {
+            console.log(`Error adding meal to the database ... ${err}`);
+            res.redirect("/clerk/list-mealkits");
+        });
+});
 
 
 module.exports = router;
