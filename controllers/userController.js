269f6027dd71e792fb8d4098712ee8b726d9b1c7
asuraxo <asuraxo@gmail.com>
2023-01-25 22:19:55 -0500

    Burgers v1.0.1 + clerkController & userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,14 +6,7 @@ const mealkitModel = require("../models/meal-kitModel");
 
 router.use('/particularMealKit', express.static(path.join(__dirname, "../assets")));
 
-
-// Find a meal from the faux database.
-const findMeal = function (id) {
-    return meals.find(meal => {
-        return meal.id == id;
-    });
-}
-
+// Gate for customer
 function checkCustomer(req, res, next) {
     if (!res.locals.user) {
         // console.log("Not yet log-in as customer");
@@ -24,15 +17,19 @@ function checkCustomer(req, res, next) {
     }
 }
 
+// Find a meal from the faux database.
+const findMeal = function (id) {
+    return meals.find(meal => {
+        return meal.id == id;
+    });
+}
 
+// process purchase cart
 const prepareViewModel = function (req, message) {
-
     if (req.session && req.session.user) {
-
         let cart = req.session.cart || [];
         let cartTotal = 0;
         const hasMeals = cart.length > 0;
-
         if (hasMeals) {
             cart.forEach(cartMeal => {
                 cartMeal.subTotal=cartMeal.qty*cartMeal.meal.price;
@@ -57,8 +54,8 @@ const prepareViewModel = function (req, message) {
     }
 }
 
+// mealkit page
 router.get("/particularMealKit/:id", checkCustomer, (req, res) => {
-
     let particularMealID = req.params.id.replace(':','');
     mealkitModel.findById(particularMealID)
         .exec()
@@ -76,16 +73,14 @@ router.get("/particularMealKit/:id", checkCustomer, (req, res) => {
     });
 });
 
+// render cart view
 router.get("/cart", checkCustomer, (req, res) => {
     res.render("cart", prepareViewModel(req));
 });
 
-// Route to add a new meal to the shopping cart.
-// The ID of the meal will be specified as part of the URL.
+// customer add meal to the cart
 router.get("/add-meal/:id", (req, res) => {
-
     const mealId = req.params.id.replace(':','');
-
     let message;
     let meal ;
     let cart = (req.session.cart = req.session.cart || []);
@@ -102,37 +97,32 @@ router.get("/add-meal/:id", (req, res) => {
             }
         });
 
+        // if it is in cart already
         if (found) {
             message = "The meal was already in the cart, incremented the quantity by one.";
             console.log(message);
             res.redirect("/customer/cart");
         }
+        // push the meal to cart
         else {
-
             cart.push({
                 id: mealId,
                 qty: 1,
                 meal,
                 subTotal:meal.price
             });
-
             message = "The meal was added to the shopping cart.";
             console.log(message);
-
             res.redirect("/customer/cart");
         }
     })
 });
 
-
+// remove meal kit from the pruchase cart
 router.get("/remove-meal/:id", checkCustomer, (req, res) => {
-
     const mealId = req.params.id.replace(':','');
-
         let cart = req.session.cart || [];
-
         const index = cart.findIndex(cartMeal => cartMeal.id == mealId);
-
         if (index >= 0) {
             message = `Removed "${cart[index].meal.name}" from the cart.`;
             cart.splice(index, 1);
@@ -140,10 +130,10 @@ router.get("/remove-meal/:id", checkCustomer, (req, res) => {
         else {
             message = "Meal was not found in the cart.";
         }
-    
     res.redirect("/customer/cart");
 });
   
+// check-out for the purchase cart
 router.get("/check-out", checkCustomer, (req, res) => {
     let cart = req.session.cart || [];
     let receipt = prepareViewModel(req);
@@ -156,17 +146,15 @@ router.get("/check-out", checkCustomer, (req, res) => {
         });
     }
 
+    // send email receipt through sendgrid
     if (cart.length > 0) {
-  
       let message = "Thank you for your purchase. Please Check email for you receipt.";
-  
       let user = req.session.user;
-
       const sgMail = require("@sendgrid/mail");
       sgMail.setApiKey(process.env.SENDGRID_API_KEY);
   
       const msg = {
-        to: user.email, //email
+        to: user.email,
         from: "wso8@myseneca.ca",
         subject: "Order Receipt",
         text: "Order",
@@ -189,18 +177,9 @@ router.get("/check-out", checkCustomer, (req, res) => {
         message
       });
     } else {
-      // There are no items in the cart.
-      // redirect to cart page, because if no items in cart, button wont show up
       console.log("invalid access");
       res.redirect("/customer/cart");
     }
 });
-
-
-
-
-
-
-
 
 module.exports = router;
